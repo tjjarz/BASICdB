fa56534e71d2ee93038c5a13c4a72d978efb95d9
tjjarz <60204884+tjjarz@users.noreply.github.com>
2020-03-09 13:18:11 -0400

    Update CharItemModels.cs

--- BasicDb.Models/CharItemModels.cs
+++ BasicDb.Models/CharItemModels.cs
@@ -10,27 +10,10 @@ using System.Threading.Tasks;
 namespace BasicDb.Models
 {
     public class GetCharItem
-    {
+    { 
         public int CharId { get; set; }
         public string Name { get; set; }
         public string ShortDescription { get; set; }
-        @ -22,21 +21,5 @@ namespace BasicDb.Models
         public string ItemDescription { get; set; }
-
-        
-        [Required]
-        public string Name { get; set; }
-
-        [Required]
-        public string ShortDescription { get; set; }
-
-        [Required]
-        public string Description { get; set; }
-
-        public List<Item> items { get; set; }
-
-        [ForeignKey(nameof(User))]
-        public string UserId { get; set; }
-        public virtual ApplicationUser User { get; set; }
     }
 }
