117fc1027d32e4791095b02e8918401e970e42f8
tjjarz <60204884+tjjarz@users.noreply.github.com>
2020-03-09 12:37:11 -0400

    Revert "Merge pull request #25 from tjjarz/TomJ3-3"
    This reverts commit ceb955edcde1e1113097411084897f2be18d6d9f, reversing
    changes made to 21fb4b3a208ffe6d078d27185a2f3118735c4670.

--- BasicDb.Data/Character.cs
+++ BasicDb.Data/Character.cs
@@ -16,27 +16,30 @@ namespace BasicDb.Data
         [Required]
         public string Name { get; set; }
 
+        /*
+        [ForeignKey(nameof(Item))]
+        public int ItemId { get; set; }*/
+        public virtual List<Item> Item { get; set; }
+        /*
+        [ForeignKey(nameof(Media))]
+        public int MediaId { get; set; }*/
+        public virtual List<Media> Media { get; set; }
+
+ /*    removed from Kerry submission
+        public virtual List<Character> Media { get; set; }*/
+
         [Required]
         public string ShortDescription { get; set; }
 
         public string Description { get; set; }
 
-        //public Guid UserId { get; set; }
-        [ForeignKey(nameof(User))]
+        //[ForeignKey(nameof(User))]
         public string UserId { get; set; }
+        //public Guid UserId { get; set; }
+
         public virtual ApplicationUser User { get; set; }
 
-        /*
-        [ForeignKey(nameof(Item))]
-        public int ItemId { get; set; }
-        public virtual List<Item> Item { get; set; }
-        
-        [ForeignKey(nameof(Media))]
-        public int MediaId { get; set; }
-        public virtual List<Media> Media { get; set; }
-
-            removed from Kerry submission
-               public virtual List<Character> Media { get; set; }*/
+        // add created and updated datetimes!
 
     }
 }
